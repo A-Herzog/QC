dc9ee30cbf962adecfbc1170099da13a0928040b
Alexander Herzog <alexander.herzog@gmx.de>
2025-05-20 08:02:55 +0200

    Faster initialization

--- docs/index.js
+++ docs/index.js
@@ -16,29 +16,36 @@ limitations under the License.
 
 /* Select language */
 import {selectLanguage} from './js/LanguageTools.js';
-selectLanguage([{name: "default", file: "index.html"}, {name: "de", file: "index_de.html"}]);
-
-/* Select color mode */
-let selectedColorMode=localStorage.getItem('selectedColorMode');
-if (selectedColorMode==null) selectedColorMode=(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches)?"dark":"light";
-document.documentElement.dataset.bsTheme=selectedColorMode;
-
-/* Init app */
 import {language} from "./js/Language.js";
 import {initApp, isDesktopApp} from './js/Main.js';
-initApp();
 
-if (isDesktopApp) {
-  const footer=document.querySelector('footer');
-  for (let link of footer.querySelectorAll("a")) if (link.href!='') {
-    const href=link.href;
-    link.onclick=()=>Neutralino.os.open(href);
-    link.removeAttribute("href");
-    link.style.cursor="pointer";
-    link.classList.add("link-primary");
+function start() {
+  /* Language */
+  if (selectLanguage([{name: "default", file: "index.html"}, {name: "de", file: "index_de.html"}])) return;
+
+  /* Select color mode */
+  let selectedColorMode=localStorage.getItem('selectedColorMode');
+  if (selectedColorMode==null) selectedColorMode=(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches)?"dark":"light";
+  document.documentElement.dataset.bsTheme=selectedColorMode;
+
+  /* Init app */
+
+  initApp();
+
+  if (isDesktopApp) {
+    const footer=document.querySelector('footer');
+    for (let link of footer.querySelectorAll("a")) if (link.href!='') {
+      const href=link.href;
+      link.onclick=()=>Neutralino.os.open(href);
+      link.removeAttribute("href");
+      link.style.cursor="pointer";
+      link.classList.add("link-primary");
+    }
+  } else {
+    const downloadA='<a id="downloadApp" target="_blank" href="https://github.com/A-Herzog/QC/releases/latest/download/QC.exe" style="display: none;"></a>';
+    const downloadButton='<button class="btn btn-primary my-1 bi-windows" onclick="document.getElementById(\'downloadApp\').click();"> '+language.GUI.downloadButton+'</button>';
+    downloadInfoArea.innerHTML="<p class='mt-3'>"+language.GUI.downloadLabel+"</p><p>"+downloadA+downloadButton+"</p>";
   }
-} else {
-  const downloadA='<a id="downloadApp" target="_blank" href="https://github.com/A-Herzog/QC/releases/latest/download/QC.exe" style="display: none;"></a>';
-  const downloadButton='<button class="btn btn-primary my-1 bi-windows" onclick="document.getElementById(\'downloadApp\').click();"> '+language.GUI.downloadButton+'</button>';
-  downloadInfoArea.innerHTML="<p class='mt-3'>"+language.GUI.downloadLabel+"</p><p>"+downloadA+downloadButton+"</p>";
 }
+
+start();
